b7182ec998a9dba2f5812dd4fc68af97a8ca1b35
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-04-23 18:46:55 +0600

    Added whatsApp chatbot

--- mental-peace-client/src/Layout/Main.jsx
+++ mental-peace-client/src/Layout/Main.jsx
@@ -1,8 +1,10 @@
+import myPic from "../assets/my pic.jpg";
 import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Shared/Header";
 import Footer from "../components/Shared/Footer";
 import "../App.css";
 import { useEffect } from "react";
+import { FloatingWhatsApp } from "react-floating-whatsapp";
 const Main = () => {
   const location = useLocation();
   useEffect(() => {
@@ -15,6 +17,14 @@ const Main = () => {
         <Outlet />
       </main>
       <Footer />
+      <FloatingWhatsApp
+        accountName="Ferdous Ahmed"
+        phoneNumber="+880 1813-240904"
+        avatar={myPic}
+        allowClickAway
+        allowEsc
+        notificationSound
+      />
     </>
   );
 };
